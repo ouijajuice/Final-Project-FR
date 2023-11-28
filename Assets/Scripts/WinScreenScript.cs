934af8915f3f8b4211cf64c18acc26173c3a7650
100bpm <143532808+100bpm@users.noreply.github.com>
2023-11-28 14:26:06 -0500

    added win screen

---
+++ Assets/Scripts/WinScreenScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreenScript : MonoBehaviour
+{
+
+
+    public GameObject winScreenObject;
+    public string playerTag = "Player";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+      if (other.CompareTag(playerTag)) 
+        {
+
+            //win screen is shown 
+            winScreenObject.SetActive(true);
+            //timescale is set to 0, pausing game
+            Time.timeScale = 0f;
+
+
+        }
+    }
+
+
+
+}
